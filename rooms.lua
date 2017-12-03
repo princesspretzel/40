fc15c52b9a9f6287c590ef0299cb7a1b27e79af5
princesspretzel <brittanga@gmail.com>
2017-12-03 13:02:48 -0800

    add field art

--- rooms.lua
+++ rooms.lua
@@ -19,14 +19,15 @@ local Rooms = {
     Feature('the sun', '/images/happysunt.png', false, true, false, true, 700, 50),
     Feature('clouds', '/images/cloudst.png', false, true, false, true, 100, 200),
   }, fieldFeatures = {
-    -- Feature('tree', '/images/treet.png', false, true, false, true, 300, 300),
-    -- Feature('other tree', '/images/othertreet.png', false, true, false, true, 600, 600),
-    -- Feature('grass1', '/images/grass1t.png', false, true, false, true, 800, 100),
-    -- Feature('grass2', '/images/grass2t.png', false, true, false, true, 400, 500),
-    -- Feature('grass3', '/images/grass3t.png', false, true, false, true, 600, 200),
-    -- Feature('flower1', '/images/flower1t.png', true, true, true, true, 700, 100),
-    -- Feature('flower2', '/images/flower2t.png', true, true, true, true, 200, 800),
-    -- Feature('pretty rock', '/images/prettyrockt.png', true, true, true, true, 500, 500)
+    Feature('tree', '/images/treet.png', false, true, false, true, 300, 300),
+    Feature('other tree', '/images/othertreet.png', false, true, false, true, 900, 200),
+    Feature('grass1', '/images/grass1t.png', false, true, false, true, 800, 100),
+    Feature('grass2', '/images/grass2t.png', false, true, false, true, 200, 200),
+    Feature('grass3', '/images/grass3t.png', false, true, false, true, 600, 200),
+    Feature('flower1', '/images/flower1t.png', true, true, true, true, 700, 100),
+    Feature('flower2', '/images/flower2t.png', true, true, true, true, 800, 300),
+    Feature('pretty rock', '/images/prettyrockt.png', true, true, true, true, 550, 550),
+    Feature('rose bush', '/images/rosebusht.png', true, true, true, true, 0, 400)
   }, desertFeatures = {
     Feature('sandblob', '/images/sandblobt.png', false, true, false, true, 650, 50),
     Feature('dunes', '/images/dunest.png', false, true, false, true, 200, 200),
